cfae21f769247fc0de87b691a143ca08abc98c5b
Pehr Hansson <pehr.hansson@parkster.se>
2022-11-27 14:42:57 +0100

    feat: Added TextField for entering guess.

--- androidApp/src/main/java/se/phan/redacted/android/feature/game/GameLayout.kt
+++ androidApp/src/main/java/se/phan/redacted/android/feature/game/GameLayout.kt
@@ -1,11 +1,19 @@
 package se.phan.redacted.android.feature.game
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import se.phan.redacted.android.ui.component.Background
@@ -15,11 +23,16 @@ import se.phan.redacted.android.ui.theme.ApplicationTheme
 fun GameLayout(title: String, text: String) {
     ApplicationTheme {
         Background {
-            Column(
-                modifier = Modifier.verticalScroll(rememberScrollState())
-            ) {
-                GameTitle(title)
-                GameText(text)
+            Box {
+                Column(
+                    modifier = Modifier.verticalScroll(rememberScrollState())
+                ) {
+                    GameTitle(title)
+                    GameText(text)
+                }
+                GuessTextField(
+                    modifier = Modifier.align(Alignment.BottomStart).fillMaxWidth()
+                )
             }
         }
     }
@@ -33,6 +46,19 @@ private fun GameTitle(title: String) {
 @Composable
 private fun GameText(text: String) {
     Text(text = text)
+}
+
+@Composable
+private fun GuessTextField(modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        modifier = modifier,
+        value = text,
+        onValueChange = { newText ->
+            text = newText
+        }
+    )
 }
 
 @Preview
